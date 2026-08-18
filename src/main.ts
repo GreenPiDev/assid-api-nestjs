eba23026cda11ae4a3786de6bb01cf1705881c77
Mustafa Akagündüz <akagunduzmustafa00@gmail.com>
2026-08-18 09:21:57 +0300

    ortam değişkenleri formatı değşikliği

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
+  const corsOrigins = (process.env.CORS_ORIGIN ?? 'http://localhost:5173')
+    .split(',')
+    .map((origin) => origin.trim());
   app.enableCors({
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:5173',
+    origin: corsOrigins,
     credentials: true,
   });
   app.setGlobalPrefix('api');
